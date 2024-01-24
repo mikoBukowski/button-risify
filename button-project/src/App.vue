6112df1b5e6ee8ae20b6b672ba7ef52906c32183
Mikolaj Bukowski <mikolaj.bukowski9@gmail.com>
2024-01-24 15:42:54 +0100

    [feat] init

---
+++ button-project/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Button from "./components/Button.vue";
+</script>
+
+<template>
+  <div>
+    <a target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <Button tag="button" size="small" variant="filled">Button</Button>
+  <Button tag="a" size="regular" variant="outlined">Button</Button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ button-project/src/components/Button.vue
@@ -0,0 +1,63 @@
+<template>
+  <component
+    :is="buttonType"
+    :class="[
+      'btn',
+      `btn-${size}`,
+      `btn-${variant}`,
+      { 'btn-block': block, 'btn-disabled': disabled },
+    ]"
+    @click="handleClick"
+  >
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { defineProps, PropType } from "vue";
+
+const props = defineProps({
+  tag: {
+    type: String as PropType<"button" | "a" | "router-link">,
+    default: "button",
+  },
+  size: {
+    type: String as PropType<"small" | "regular" | "large">,
+    default: "regular",
+  },
+  variant: {
+    type: String as PropType<"filled" | "outlined">,
+    default: "filled",
+  },
+  block: Boolean,
+  disabled: Boolean,
+});
+
+const buttonType = computed(() => (props.disabled ? "button" : props.tag));
+
+const handleClick = (event: Event) => {
+  if (!props.disabled) {
+    // emit("click", event);
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+}
+.btn-small {
+}
+.btn-regular {
+}
+.btn-large {
+}
+.btn-filled {
+}
+.btn-outlined {
+}
+.btn-block {
+}
+.btn-disabled {
+}
+</style>
